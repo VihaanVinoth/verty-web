08851e9346ea3340b5851e69231817e782497e36
Vihaan Vinoth <vertialX1000@outlook.com.au>
2026-09-13 17:07:24 +1000

    fix extension

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  base: '/verty-web/',
-})
+export default defineConfig(({ mode }) => ({
+  base: mode === 'extension' ? './' : '/verty-web/',
+}))
